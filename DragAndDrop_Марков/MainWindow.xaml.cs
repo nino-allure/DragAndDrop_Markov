da7fde0e90a82ada378314eb19401280b8236164
nino-allure <kxzuy@vk.com>
2026-05-15 19:40:00 +0500

    Создание таймера. Марков

--- DragAndDrop_Марков/MainWindow.xaml.cs
+++ DragAndDrop_Марков/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DragAndDrop_Марков
 {
@@ -16,9 +17,26 @@ namespace DragAndDrop_Марков
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer dispatcherTimer;
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new System.TimeSpan(0, 0, 0, 0, 1000 / 60);
         }
+        private void DispatcherTimer_Tick(object sender, System.EventArgs e)
+        {
+
+        }
+        private void Image_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
     }
 }
